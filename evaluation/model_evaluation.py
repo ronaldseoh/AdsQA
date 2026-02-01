515522a7626211fecb50e4aef012c25b3c5962e7
Ronald Seoh <ronaldseoh@icloud.com>
2026-02-01 16:13:01 -0500

    Revert "custom openai api base"
    This reverts commit 34a6e4de0116a16721352ec2f56ca0ebbafd8e56.

--- evaluation/model_evaluation.py
+++ evaluation/model_evaluation.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
             api_key=os.environ.get("AZURE_OPENAI_API_KEY")
         )
     else:
-        client = openai.OpenAI(api_base=os.environ.get("OPENAI_API_BASE"))
+        client = openai.OpenAI()
 
     strict_acc_scores = {"Type_1": 0, "Type_2": 0, "Type_3": 0, "Type_4": 0, "Type_5": 0}
     strict_acc_counts = {"Type_1": 0, "Type_2": 0, "Type_3": 0, "Type_4": 0, "Type_5": 0}
